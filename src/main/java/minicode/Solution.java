0f25fb74c69892a600702e082d7f98748713a7c3
TKLatWork <sky71767@gmail.com>
2020-02-17 14:31:50 +0800

    简化，把那边我的搬过来了

---
+++ src/main/java/minicode/Solution.java
@@ -0,0 +1,56 @@
+package minicode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+    static String[] map = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    public List<String> letterCombinations(String digits) {
+        return letterCombinations(toIntArray(digits));
+    }
+
+    public List<String> letterCombinations(Integer[] digits) {
+        return letterCombinations(Arrays.asList(digits));
+    }
+
+    public List<String> letterCombinations(List<Integer> seq) {
+        result = new ArrayList<>();
+        if(seq.size() > 0){
+            re("", seq, 0);
+        }
+        return result;
+    }
+
+
+    private List<String> result;
+
+    private void re(String prefix, List<Integer> seq, Integer index){
+        String cChars = map[seq.get(index)];
+        for (int i = 0; i < cChars.length(); i++) {
+            String newPrefix = prefix + cChars.charAt(i);
+            if(seq.size() > index + 1){
+                re(newPrefix, seq, index + 1);
+            }else{
+                result.add(newPrefix);
+            }
+        }
+    }
+
+    //util
+    private List<Integer> toIntArray(String digits){
+        List<Integer> seq = new ArrayList<>();
+        for (int i = 0; i < digits.length(); i++) {
+            try {
+                int index = Character.getNumericValue(digits.charAt(i));
+                seq.add(index);
+            }catch (NumberFormatException e){
+
+            }
+        }
+        return seq;
+    }
+
+}
--- src/main/java/minicode/Test.java
+++ src/main/java/minicode/Test.java
@@ -1,35 +1,61 @@
 package minicode;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Test {
 
     public static void main(String[] args) throws Exception {
-        LetterConvert convert = new LetterConvert();
-
-        Integer[] digits = new Integer[]{9};
-        convert.resolveAndPrint(digits);
-        Test.Check(convert.getPrint(), Arrays.asList("W", "X", "Y", "Z"));
-
-        digits = new Integer[]{2, 3};
-        convert.resolveAndPrint(digits);
-        Test.Check(convert.getPrint(), Arrays.asList("AD", "AE", "AF", "BD", "BE", "BF", "CD", "CE", "CF"));
-
-        digits = new Integer[]{23};
-        convert.resolveAndPrint(digits);
-        Test.Check(convert.getPrint(), Arrays.asList("AD", "AE", "AF", "BD", "BE", "BF", "CD", "CE", "CF"));
-
-        digits = new Integer[]{56};
-        convert.resolveAndPrint(digits);
-        System.out.println(convert.getPrint());
+        questionOne();
+        questionTwo();
     }
 
-    public static void Check(List<String> src, List<String> match) throws Exception {
-        if(src.size() != match.size() || !src.containsAll(match)){
-            System.out.println(src);
-            System.out.println(match);
-            throw new Exception("Check fail");
+    public static void questionOne(){
+        Solution convert = new Solution();
+        Map<List<Integer>, List<String>> testData = new HashMap<>();
+        testData.put(Arrays.asList(0), Arrays.asList());
+        testData.put(Arrays.asList(1), Arrays.asList());
+        testData.put(Arrays.asList(2), Arrays.asList("a", "b", "c"));
+        testData.put(Arrays.asList(3), Arrays.asList("d", "e", "f"));
+        testData.put(Arrays.asList(4), Arrays.asList("g", "h", "i"));
+        testData.put(Arrays.asList(5), Arrays.asList("j", "k", "l"));
+        testData.put(Arrays.asList(6), Arrays.asList("m", "n", "o"));
+        testData.put(Arrays.asList(7), Arrays.asList("p", "q", "r", "s"));
+        testData.put(Arrays.asList(8), Arrays.asList("t", "u", "v"));
+        testData.put(Arrays.asList(9), Arrays.asList("w", "x", "y", "z"));
+        //test
+        testData.forEach((k,v) -> {
+            List<String> result = convert.letterCombinations(k);
+            Test.CheckAndPrint(k, v, result);
+        });
+    }
+
+    public static void questionTwo(){
+        Solution convert = new Solution();
+        Map<List<Integer>, List<String>> testData = new HashMap<>();
+        testData.put(Arrays.asList(2, 3),
+                Arrays.asList("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"));
+        testData.put(Arrays.asList(8, 8),
+                Arrays.asList("tt", "tu", "tv", "ut", "uu", "uv", "vt", "vu", "vv"));
+        //test
+        testData.forEach((k,v) -> {
+            List<String> result = convert.letterCombinations(k);
+            Test.CheckAndPrint(k, v, result);
+        });
+    }
+
+    public static void CheckAndPrint(List<Integer> input,
+                                     List<String> expect,
+                                     List<String> actual) {
+        System.out.println("Checking:" + Arrays.toString(input.toArray()));
+        if(expect.equals(actual)){
+            System.out.println(Arrays.toString(expect.toArray()));
+        }else{
+            System.out.println(Arrays.toString(expect.toArray()));
+            System.out.println(Arrays.toString(actual.toArray()));
+            System.out.println("Check Fail:");
         }
     }
 
